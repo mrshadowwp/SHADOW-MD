f345247d923738f0abadb675276dc8f97a7ff92a
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-09-08 09:04:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "o7NEVQzY#7QxI3jZN08p4cxJ1T9tSotuoZoI5qO-y4q7IdB19lpI",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68de.jpg",
 SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
