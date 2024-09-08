a17420ad9b989ab2641bfe6aa7551ace536877f5
mrshadowwp <nirmalasanjeevani556@gmail.com>
2024-09-08 19:30:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "HaxmARQS#K0_79_-DHmxJHP9_zFHItr6-Y-FnEsHNw6cReFc_uW8",
+SESSION_ID: process.env.SESSION_ID || "VBxVGRxb#p-qcrKKysRef4piG7Tz4aRCepOjJ5f1_ItpKGQQjpbA",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68de.jpg",
-SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+SUDO_NB: process.env.SUDO_NB || "+94716304018",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+MODE: process.env.MODE || "private",
 AUTO_VOICE: "true"
 };
